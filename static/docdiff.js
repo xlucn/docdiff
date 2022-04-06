3ccb3cb9d50c298dbf23a887ff2afa766f00c2aa
Lu Xu <oliver_lew@outlook.com>
2022-04-06 14:15:58 +0800

    some refactoring, and put event bindings to js file

--- static/docdiff.js
+++ static/docdiff.js
@@ -1,43 +1,48 @@
-function cacheInput() {
-    let boxes = document.getElementsByClassName("prefs");
-    for (let i = 0; i < boxes.length; i++) {
-        localStorage.setItem(boxes[i].id, boxes[i].checked);
-    }
-
-    let bg = localStorage.getItem('bg') == "true";
-    let fg = localStorage.getItem('fg') == "true";
-    let ul = localStorage.getItem('ul') == "true";
+function updatePreview() {
+    let bg = localStorage.getItem("bg") == "true";
+    let fg = localStorage.getItem("fg") == "true";
+    let ul = localStorage.getItem("ul") == "true";
 
     let spans = document.getElementsByClassName("text-add");
     for (let i = 0; i < spans.length; i++) {
-        spans[i].style.backgroundColor = bg ? "yellow" : "white";
-        spans[i].style.color = fg ? "blue" : "black";
-        spans[i].style.textDecoration = ul ? "underline blue" : "";
+        spans[i].style.backgroundColor = bg ? "yellow" : "unset";
+        spans[i].style.color = fg ? "blue" : "unset";
+        spans[i].style.textDecorationLine = ul ? "underline" : "";
+        spans[i].style.textDecorationColor = ul ? "blue" : "";
     }
 
     spans = document.getElementsByClassName("text-del");
     for (let i = 0; i < spans.length; i++) {
-        spans[i].style.backgroundColor = bg ? "yellow" : "white";
-        spans[i].style.color = fg ? "red" : "black";
-        spans[i].style.textDecoration = ul ? "underline red wavy" : "";
+        spans[i].style.backgroundColor = bg ? "yellow" : "unset";
+        spans[i].style.color = fg ? "red" : "unset";
+        spans[i].style.textDecorationLine = ul ? "underline" : "";
+        spans[i].style.textDecorationColor = ul ? "red" : "";
+        spans[i].style.textDecorationStyle = ul ? "wavy" : "";
     }
 }
 
-function resetInput() {
+function cacheSettings() {
     let boxes = document.getElementsByClassName("prefs");
     for (let i = 0; i < boxes.length; i++) {
-        localStorage.removeItem(boxes[i].id);
+        localStorage.setItem(boxes[i].id, boxes[i].checked);
     }
+    updatePreview();
+}
+
+function resetSettings() {
+    let boxes = document.getElementsByClassName("prefs");
+    for (let i = 0; i < boxes.length; i++) {
+        localStorage.setItem(boxes[i].id, boxes[i].defaultChecked);
+    }
+    updatePreview();
 }
 
 window.onload = function() {
     let boxes = document.getElementsByClassName("prefs");
     for (let i = 0; i < boxes.length; i++) {
-        let v = localStorage.getItem(boxes[i].id);
-        if (v == "true") {
-            boxes[i].checked = true;
-        } else if (v == "false") {
-            boxes[i].checked = false;
-        }
+        boxes[i].checked = eval(localStorage.getItem(boxes[i].id));
+        boxes[i].addEventListener("change", cacheSettings);
     }
+    updatePreview();
+    document.querySelector("input[type=reset]").addEventListener("click", resetSettings);
 }
